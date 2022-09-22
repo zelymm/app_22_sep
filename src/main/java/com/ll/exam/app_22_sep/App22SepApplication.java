e177b0b7c15c5dd8231419ac54b8b945d8e0a6c4
zelymm <dkdlwkrz@naver.com>
2022-09-22 10:51:11 +0900

    [feat]: set up as a batch app

--- src/main/java/com/ll/exam/app_22_sep/App22SepApplication.java
+++ src/main/java/com/ll/exam/app_22_sep/App22SepApplication.java
@@ -1,9 +1,11 @@
 package com.ll.exam.app_22_sep;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class App22SepApplication {
 
     public static void main(String[] args) {
