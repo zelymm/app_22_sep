699988ba204872b72a546593aaf5123bb50cd36b
zelymm <dkdlwkrz@naver.com>
2022-09-22 09:34:02 +0900

    initial commit

---
+++ src/main/java/com/ll/exam/app_22_sep/App22SepApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app_22_sep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App22SepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App22SepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/exam/app_22_sep/App22SepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app_22_sep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App22SepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
