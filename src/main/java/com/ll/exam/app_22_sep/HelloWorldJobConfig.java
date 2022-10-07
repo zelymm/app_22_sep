15cf727652ddb57d22fcd4383c9dcdcadcd8f911
zelymm <dkdlwkrz@naver.com>
2022-10-07 17:15:39 +0900

    [fix]: execute that a different ID is assigned each time

--- src/main/java/com/ll/exam/app_22_sep/HelloWorldJobConfig.java
+++ src/main/java/com/ll/exam/app_22_sep/HelloWorldJobConfig.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,7 @@ public class HelloWorldJobConfig {
     @Bean
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
+                .incrementer(new RunIdIncrementer())
                 .start(helloWorldStep1())
                 .build();
     }
