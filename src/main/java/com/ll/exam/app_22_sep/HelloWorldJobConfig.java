7777cf8dc74eac50ba09601d8a8c68daaf85cc8d
zelymm <dkdlwkrz@naver.com>
2022-10-07 17:26:38 +0900

    feat: add Scope

--- src/main/java/com/ll/exam/app_22_sep/HelloWorldJobConfig.java
+++ src/main/java/com/ll/exam/app_22_sep/HelloWorldJobConfig.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -27,6 +29,7 @@ public class HelloWorldJobConfig {
     }
 
     @Bean
+    @JobScope
     public Step helloWorldStep1() {
         return stepBuilderFactory.get("helloWorldStep1")
                 .tasklet(helloWorldTasklet())
@@ -34,6 +37,7 @@ public class HelloWorldJobConfig {
     }
 
     @Bean
+    @StepScope
     public Tasklet helloWorldTasklet() {
         return (contribution, chunkContext) -> {
             System.out.println("hello world!");
