a4d1831273a3d3b65e8a167051075f232741479c
zelymm <dkdlwkrz@naver.com>
2022-10-07 16:51:49 +0900

    feat: run helloWorldJob

---
+++ src/main/java/com/ll/exam/app_22_sep/HelloWorldJobConfig.java
@@ -0,0 +1,41 @@
+package com.ll.exam.app_22_sep;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloWorldJobConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private  final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloWorldJob() {
+        return jobBuilderFactory.get("helloWorldJob")
+                .start(helloWorldStep1())
+                .build();
+    }
+
+    @Bean
+    public Step helloWorldStep1() {
+        return stepBuilderFactory.get("helloWorldStep1")
+                .tasklet(helloWorldTasklet())
+                .build();
+    }
+
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return (contribution, chunkContext) -> {
+            System.out.println("hello world!");
+            return RepeatStatus.FINISHED;
+        };
+    }
+}
